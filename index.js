d65bf73d4bafb14c9d536fc5c5787c3312ac7c46
Vanderllee <vanderllee@gmail.com>
2021-10-04 10:14:35 -0300

    cors

--- index.js
+++ index.js
@@ -7,20 +7,22 @@ const authRoute = require('./routes/auth')
 const userRoute = require('./routes/users')
 const postRoute = require('./routes/posts')
 const categoryRoute = require('./routes/categories')
+const cors = require("cors")
 
 const multer = require('multer')
 const path = require("path")
 
 
 dotenv.config()
-app.use(express.json())
 
+app.use(cors())
+app.use(express.json())
 app.use("/images", express.static(path.join(__dirname, "/images")))
 
 mongoose.connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-   
+    
 }).then(console.log('Connected to MongoDB...')).catch((error) => console.log(error));
 
 const storage = multer.diskStorage({
@@ -38,7 +40,6 @@ const upload = multer({ storage: storage })
 app.post('/api/upload', upload.single("file"), (req, res) => {
     res.status(200).json('Arquivo inserido!')
 })
-
 app.use('/api/auth', authRoute)
 app.use('/api/users', userRoute)
 app.use('/api/posts', postRoute)
